c266b2c8450e1e070c55e32b403dbc452cd6e945
Hatatori Nitsukawa <kcireh.i@gmail.com>
2021-12-29 18:52:19 -0300

    Add files via upload

--- foreach/foreach.js
+++ foreach/foreach.js
@@ -1,12 +1,11 @@
-dados = [
-  {nome:"nome A", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/81f46a4161be895721f955bccec9ae15.jpeg"},
-  {nome:"nome B", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/f678cfd7bca9c04294d7b364dc5e7421.jpeg"},
-  {nome:"nome C", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/88b4e45069ce92f267d755152d559ff6.jpeg"},
-  {nome:"nome D", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/ad236ba09a9b3928db976342ae49805f.jpeg"},
-]
+// dados = [
+//   {nome:"nome A", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/81f46a4161be895721f955bccec9ae15.jpeg"},
+//   {nome:"nome B", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/f678cfd7bca9c04294d7b364dc5e7421.jpeg"},
+//   {nome:"nome C", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/88b4e45069ce92f267d755152d559ff6.jpeg"},
+//   {nome:"nome D", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/ad236ba09a9b3928db976342ae49805f.jpeg"},
+// ]
 
 function rep(div, dados){
-  // const div = estrutura
   k = []
   dados.map(el=>{
     t = div.cloneNode(true)

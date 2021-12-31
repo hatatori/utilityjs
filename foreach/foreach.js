cca3c48db4c58f9f224f66aa56add7b0c7535ac3
Hatatori Nitsukawa <kcireh.i@gmail.com>
2021-12-31 07:46:56 -0300

    Add files via upload

--- foreach/foreach.js
+++ foreach/foreach.js
@@ -1,9 +1,29 @@
-// dados = [
-//   {nome:"nome A", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/81f46a4161be895721f955bccec9ae15.jpeg"},
-//   {nome:"nome B", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/f678cfd7bca9c04294d7b364dc5e7421.jpeg"},
-//   {nome:"nome C", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/88b4e45069ce92f267d755152d559ff6.jpeg"},
-//   {nome:"nome D", "img":"https://beta.crunchyroll.com/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/ad236ba09a9b3928db976342ae49805f.jpeg"},
-// ]
+dados = [
+  {
+    nome:"Demons layer", 
+    img: "https://img1.ak.crunchyroll.com/i/spire4-tmb/a185fe104556794d24af2164602f23d31639907206_wide.jpg"
+  },
+  {
+    nome:"One piece", 
+    img: "https://img1.ak.crunchyroll.com/i/spire3-tmb/42b6b8dba929b12af77df312e375e0c01639876517_wide.jpg"
+  },
+  {
+    nome:"Boruto", 
+    img: "https://img1.ak.crunchyroll.com/i/spire2-tmb/38b0099781a3905f4c38259fa69ab53f1639872130_wide.jpg"
+  },
+  {
+    nome:"Sakugan", 
+    img: "https://img1.ak.crunchyroll.com/i/spire1-tmb/38766716967399fd639b4dc63489bfe01639649308_wide.jpg"
+  },
+  {
+    nome:"Digimon", 
+    img: "https://img1.ak.crunchyroll.com/i/spire3-tmb/3ca0c8351d39123365c8f3274d5bf6d61639874856_wide.jpg"
+  },
+  {
+    nome:"Restaurant", 
+    img: "https://img1.ak.crunchyroll.com/i/spire4-tmb/5e581d26880d07893faa457d00abe7351639724593_wide.jpg"
+  }
+]
 
 function rep(div, dados){
   k = []
